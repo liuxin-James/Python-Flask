e98112c2649fa2fdedbb5ca1249368f38661cc61
liuxin-James <xl9jv@virginia.edu>
2022-10-24 22:55:50 -0400

    Add migrations

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify
 from flask_smorest import Api
-
-from flask import Flask
-from flask_smorest import Api
+from flask_migrate import Migrate
 
 from db import db
 
@@ -14,6 +12,8 @@ from resources.tag import blp as TagBlueprint
 
 from flask_jwt_extended import JWTManager
 from resources.user import blp as UserBlueprint
+
+from blocklist import BLOCKLIST
 
 
 def create_app(db_url=None):
@@ -31,10 +31,24 @@ def create_app(db_url=None):
         "DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "My name is Xin Liu at UVA"
     jwt = JWTManager(app)
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
 
     @jwt.additional_claims_loader
     def add_claims_to_jwt(identity):
@@ -71,8 +85,19 @@ def create_app(db_url=None):
             401,
         )
 
-    with app.app_context():
-        db.create_all()
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.",
+                    "error": "token_revoked"}
+            ),
+            401,
+        )
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
--- resources/user.py
+++ resources/user.py
@@ -1,8 +1,14 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
-
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt,
+    jwt_required,
+    create_refresh_token,
+    get_jwt_identity
+)
+from blocklist import BLOCKLIST
 
 from db import db
 from models import UserModel
@@ -59,7 +65,29 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            access_token = create_access_token(identity=user.id)
-            return {"access_token": access_token}, 200
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
 
         abort(401, message="Invalid credentials.")
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        # Make it clear that when to add the refresh token to the blocklist will depend on the app design
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"access_token": new_token}, 200
